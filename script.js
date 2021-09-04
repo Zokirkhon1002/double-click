c2c4586b84bb6895ca81db405d8041e66a4bee25
Zokirkhon <78657264+Zokirkhon1002@users.noreply.github.com>
2021-09-05 00:21:34 +0900

    asf

---
+++ script.js
@@ -0,0 +1,10 @@
+let becomeKampot = document.querySelector(".becomeKampot"),
+    times = document.getElementById("times");
+
+becomeKampot.addEventListener("dblclick", (e)=> createImgKampot(e));
+
+function createImgKampot(e){
+    const
+}
+
+
