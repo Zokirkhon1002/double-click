a521e94457a2ff7a40ce891a837f1cec68c4468c
Zokirkhon <78657264+Zokirkhon1002@users.noreply.github.com>
2021-09-05 01:17:56 +0900

    asf

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
 let becomeKampot = document.querySelector(".becomeKampot"),
-    times = document.getElementById("times");
+  times = document.getElementById("times");
+let clickedTimes = 1;
+becomeKampot.addEventListener("dblclick", (e) => {
+   createImgKampot(e);
+});
 
-becomeKampot.addEventListener("dblclick", (e)=> createImgKampot(e));
+// .addEventListener('click', (e) => {
+//     if(clickTime === 0) {
+//         clickTime = new Date().getTime()
+//     } else {
+//         if((new Date().getTime() - clickTime) < 800) {
+//             createImgKampot(e)
+//             clickTime = 0
+//         } else {
+//             clickTime = new Date().getTime()
+//         }
+//     }
+// })
 
-function createImgKampot(e){
-    const
+function createImgKampot(e) {
+  const img = document.createElement("img");
+  img.src = "https://image.flaticon.com/icons/png/512/2738/2738804.png";
+  img.alt = "Kampot";
+  img.title = "Kampot";
+  img.classList.add("kampot");
+
+  const x = e.clientX;
+  const y = e.clientY;
+
+  const leftOffset = e.target.offsetLeft;
+  const topOffset = e.target.offsetTop;
+
+  const xInside = x - leftOffset;
+  const yInside = y - topOffset;
+
+  img.style.left = `${xInside}px`;
+  img.style.top = `${yInside}px`;
+
+ becomeKampot.appendChild(img);
+  times.innerHTML = clickedTimes++;
+
+
+
+  setTimeout(() => img.remove(), 3000);
+
+  
 }
-
-
